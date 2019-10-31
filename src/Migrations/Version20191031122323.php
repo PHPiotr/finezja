c6a944665cfe77091cab2df89620e1fe196c4d8f
Piotr Kowalski <piet.kowalski@gmail.com>
2019-10-31 15:09:05 +0100

    Rename table category to categories

--- src/Migrations/Version20191031122323.php
+++ src/Migrations/Version20191031122323.php
@@ -22,7 +22,7 @@ final class Version20191031122323 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE category ADD sort INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE categories ADD sort INT DEFAULT NULL');
     }
 
     public function down(Schema $schema) : void
@@ -30,6 +30,6 @@ final class Version20191031122323 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE category DROP sort');
+        $this->addSql('ALTER TABLE categories DROP sort');
     }
 }
