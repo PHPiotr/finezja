f49187895dd5ac337b4835e5b442a4ce8285a34b
Piotr Kowalski <piet.kowalski@gmail.com>
2019-10-31 10:18:58 +0100

    Add masonry-like layout to offers pages

--- src/Controller/OfferController.php
+++ src/Controller/OfferController.php
@@ -20,9 +20,35 @@ class OfferController extends AbstractController
         }
         $title = $item['title'];
         $description = $item['description'];
+
+        $imagesLeft = [];
+        $imagesMiddle = [];
+        $imagesRight = [];
+        $images = $item['images'];
+        $imagesLength = count($images);
+        $i = 0;
+        while ($i < $imagesLength) {
+            $left = $i;
+            $middle = $i + 1;
+            $right = $i + 2;
+            if (isset($images[$left])) {
+                $imagesLeft[] = $images[$left];
+            }
+            if (isset($images[$middle])) {
+                $imagesMiddle[] = $images[$middle];
+            }
+            if (isset($images[$right])) {
+                $imagesRight[] = $images[$right];
+            }
+            $i += 3;
+        }
+
         return $this->render('offer.html.twig', [
             'active' => 'offer',
             'item' => $item,
+            'images_left' => $imagesLeft,
+            'images_middle' => $imagesMiddle,
+            'images_right' => $imagesRight,
             'metaTitle' => $title,
             'metaDescription' => sprintf('%s.%s', $title, !empty($description) ? " $description." : ''),
             'metaKeywords' => $title . ',',
@@ -34,7 +60,7 @@ class OfferController extends AbstractController
         $metaDescription = '';
         $metaKeywords = '';
         $index = 0;
-        $items = (new Offer())->getItems();
+        $items = array_values((new Offer())->getItems());
         foreach ($items as $item) {
             $lowerTitle = mb_strtolower($item['title']);
             $metaKeywords .= $lowerTitle . ',';
@@ -44,9 +70,31 @@ class OfferController extends AbstractController
             $metaDescription .= $lowerTitle . ', ';
         }
         $metaDescription = rtrim($metaDescription, ', ');
+
+        $itemsLeft = $itemsMiddle = $itemsRight = [];
+        $itemsCount = count($items);
+        $i = 0;
+        while($i < $itemsCount) {
+            $left = $i;
+            $middle = $i + 1;
+            $right = $i + 2;
+            if (isset($items[$left])) {
+                $itemsLeft[] = $items[$left];
+            }
+            if (isset($items[$middle])) {
+                $itemsMiddle[] = $items[$middle];
+            }
+            if (isset($items[$right])) {
+                $itemsRight[] = $items[$right];
+            }
+            $i += 3;
+        }
+
         return $this->render('offers.html.twig', [
             'active' => 'offer',
-            'items' => $items,
+            'items_left' => $itemsLeft,
+            'items_middle' => $itemsMiddle,
+            'items_right' => $itemsRight,
             'metaTitle' => 'Oferta',
             'metaDescription' => ucfirst($metaDescription) . '.',
             'metaKeywords' => $metaKeywords,
