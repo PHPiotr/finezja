02cea5f80b9d6a868d1ea7e7acb72babc5b293a0
Piotr Kowalski <piet.kowalski@gmail.com>
2020-02-16 16:07:57 +0100

    Automate events display based on date

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,11 +3,39 @@
 namespace App\Controller;
 
 use App\Entity\Category;
+use Carbon\Carbon;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Filesystem\Filesystem;
 
 class HomeController extends AbstractController
 {
+    private function getEvent()
+    {
+        $events = [
+            '21.01' => '21.01 Dzień Babci<br />22.01 Dzień Dziadka',
+            '22.01' => '22.01 Dzień Dziadka',
+            '14.02' => '14.03 Walentynki',
+            '08.03' => '08.03 Dzień Kobiet',
+            '13.04' => '12-13.04 Wielkanoc',
+            '26.05' => '26.05 Dzień Matki',
+            '23.06' => '26.05 Dzień Ojca',
+            '30.09' => '30.09 Dzień Chłopaka',
+            '14.10' => '14.10 Dzień Nauczyciela',
+            '01.11' => '01.11 Wszystkich Świętych',
+            '26.12' => '24-26.12 Boże Narodzenie',
+        ];
+
+        $currentEvent = null;
+        foreach ($events as $eventKey => $eventValue) {
+            $eventDate = Carbon::parse(Carbon::createFromFormat('d.m H:i:s', $eventKey . ' 23:59:59')->toDateString());
+            if ($eventDate->gte(Carbon::now()->subDay())) {
+                return $eventValue;
+            }
+        }
+
+        return $events[array_keys($events)[0]];
+    }
+
     public function indexAction()
     {
         $repo = $this->getDoctrine()->getRepository(Category::class);
@@ -43,6 +71,7 @@ class HomeController extends AbstractController
         }
 
         return $this->render('home.html.twig', [
+            'event' => $this->getEvent(),
             'active' => 'home',
             'metaTitle' => 'Kwiaciarnia Finezja - Wodzisław Śląski',
             'metaDescription' => 'Bukiety ślubne, palmy pogrzebowe, stroiki, kompozycje, rośliny doniczkowe.',
